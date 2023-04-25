66bb396f915bde16cf49d975bddaeee4af0f567d
murilonp <murilloprado457@gmail.com>
2023-04-25 18:06:10 -0300

    first commit

---
+++ src/use-cases/register.ts
@@ -0,0 +1,34 @@
+import { prisma } from '@/lib/prisma';
+import { hash } from 'bcryptjs';
+
+interface RegisterUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: any) {}
+
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6);
+
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exists.');
+    }
+
+    // const prismaUsersRepository = new PrismaUsersRepository();
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    });
+  }
+}
