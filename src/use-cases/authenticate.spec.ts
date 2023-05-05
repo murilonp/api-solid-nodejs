e380e528fec3e228d2643575aa3b3d9a27759902
murilonp <murilloprado457@gmail.com>
2023-05-05 16:57:54 -0300

    use-cases-check-in

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -28,7 +28,7 @@ describe('Authenticate Use Case', () => {
   });
 
   it('should not be able to authenticate with wrong email', async () => {
-    expect(() =>
+    await expect(() =>
       sut.execute({
         email: 'john.doe@gmail.com',
         password: '123456',
@@ -43,7 +43,7 @@ describe('Authenticate Use Case', () => {
       password_hash: await hash('123456', 6),
     });
 
-    expect(() =>
+    await expect(() =>
       sut.execute({
         email: 'john.doe@gmail.com',
         password: '123123',
